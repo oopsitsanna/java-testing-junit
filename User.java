86cf8602022820a9cecf7ffab240b840fb88ecb5
Anna Oleksyeyenko <51105515+oopsitsanna@users.noreply.github.com>
2021-02-19 05:02:59 -0800

    Update User.java

--- User.java
+++ User.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package junit;
 
 import java.lang.String;
@@ -12,10 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-/**
- *
- * @author hakin
- */
 public class User {
     private int id;
     private String name;
